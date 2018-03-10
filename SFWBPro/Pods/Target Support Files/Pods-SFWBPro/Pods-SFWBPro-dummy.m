4c3f8b6c1127c295fc9499931b96b6b06bc87300
lushengfu <cylushengfu@163.com>
2018-03-10 10:39:13 +0800

    项目添加cocoapods

---
+++ SFWBPro/Pods/Target Support Files/Pods-SFWBPro/Pods-SFWBPro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SFWBPro : NSObject
+@end
+@implementation PodsDummy_Pods_SFWBPro
+@end
---
+++ SFWBPro/Pods/Target Support Files/Pods-SFWBProTests/Pods-SFWBProTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SFWBProTests : NSObject
+@end
+@implementation PodsDummy_Pods_SFWBProTests
+@end
